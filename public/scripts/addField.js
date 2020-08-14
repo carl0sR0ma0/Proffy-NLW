68a55726f149d23330416c5829dafbd016a0a69a
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-14 14:23:49 -0300

    Alterando a função cloneField para não deixar campos de times vazios.

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -5,15 +5,18 @@ document.querySelector("#add-time")
 
 // Executar uma acao
 function cloneField() {
-  // Duplicar os campos. Que campos??
-  const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
-  // Pegar os Campos. Que campos??
-  const fields = newFieldContainer.querySelectorAll('input')
-  // Para cada campo, limpa-lo
-  fields.forEach(function (field) {
-    // Pegar o field do momento e limpa ele
-    field.value = ""
-  })
-  // Colocar na página: Onde??
-  document.querySelector('#schedule-items').appendChild(newFieldContainer)
+  const field = document.querySelectorAll('.schedule-item input')
+  if (field[0].value !== '' && field[1].value !== '') {
+    // Duplicar os campos. Que campos??
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    // Pegar os Campos. Que campos??
+    const fields = newFieldContainer.querySelectorAll('input')
+    // Para cada campo, limpa-lo
+    fields.forEach(function (field) {
+      // Pegar o field do momento e limpa ele
+      field.value = ""
+    })
+    // Colocar na página: Onde??
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+  }
 }
